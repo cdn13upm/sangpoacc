f7ed7be5518400cc9e2472c7a6ba7a33b0e9f7d3
cdn13upm <cdn31419@gmail.com>
2026-07-24 21:51:54 +0800

    Add ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import next from "@next/eslint-plugin-next";
+
+export default [
+  { ignores: [".next"] },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      ...react.configs.flat.recommended,
+      ...react.configs.flat["jsx-runtime"],
+      ...next.configs.flat.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    settings: { react: { version: "detect" } },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/prop-types": "off",
+    },
+  },
+];
